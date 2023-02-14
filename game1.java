054bd00bb6130110e1694cba14091468cee855d3
maks <yagniskhak04@gmail.com>
2023-02-14 15:46:30 +0000

    pushing main

---
+++ game1.java
@@ -0,0 +1,13 @@
+
+public class game1
+{
+    public static void main(String[] args) {
+        GameArena arena = new GameArena(750, 750, true);
+        Goodie cat = new Goodie();
+        cat.addTo(arena);
+
+
+    
+    }
+
+}
